c19b421230889c9d56fecce1b5dfd5947e50f03c
pretentious7 <abhicherath@gmail.com>
2021-07-28 17:49:55 +0000

    added some stuff

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-emcc -s USE_REGAL=1 -Wall -g -gsource-map regal_test.C -o regal_test.js
+emcc -lRegal -Wall -g -gsource-map esUtil.c regal_test2.C -o regal_test.js -s USE_REGAL=1 
--- regal_test2.C
+++ regal_test2.C
@@ -2,3 +2,51 @@
 #include <Gl/Regal.h>
 #include <emscripten.h>
 #include <emscripten/html5.h>
+#include "esutil.h"
+#endif
+
+#include <GL/gl.h>
+#include <stdio.h>
+
+typedef struct
+{
+   // Handle to a program object
+   GLuint programObject;
+
+} UserData;
+
+void Draw(ESContext *escontext) {
+   UserData *userData = esContext->userData;
+	// Set the viewport
+   glViewport ( 0, 0, esContext->width, esContext->height );
+   
+   // Clear the color buffer
+   glClear ( GL_COLOR_BUFFER_BIT );
+    glClearColor(0,0,42,1);
+
+    printf("horte\n");
+}
+
+
+int main ( int argc, char *argv[] )
+{
+   ESContext esContext;
+   UserData  userData;
+
+   esInitContext ( &esContext );
+   esContext.userData = &userData;
+
+   esCreateWindow ( &esContext, "Hello Triangle", 320, 240, ES_WINDOW_RGB );
+	RegalMakeCurrent((RegalSystemContext)1);
+
+
+   /*
+   if ( !Init ( &esContext ) )
+      return 0;
+      */
+
+   esRegisterDrawFunc ( &esContext, Draw );
+
+   esMainLoop ( &esContext );
+}
+
