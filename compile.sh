caf1bb4e2d294e255535996c5acf491d2ad239d3
Abhishek Cherath <abhicherath@gmail.com>
2021-07-28 15:24:35 +0000

    new regal test to see if window management with egl works!

---
+++ compile.sh
@@ -0,0 +1,1 @@
+emcc -s USE_REGAL=1 -Wall -g -gsource-map regal_test.C -o regal_test.js
---
+++ regal_test2.C
@@ -0,0 +1,4 @@
+#ifdef __EMSCRIPTEN__
+#include <Gl/Regal.h>
+#include <emscripten.h>
+#include <emscripten/html5.h>
